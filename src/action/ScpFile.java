164d208fc7f1364f81e34f116d6840c98656a2f9
Dongliang Yu <yudl.nju@gmail.com>
2015-03-29 17:22:30 +0800

    add remote scp file

---
+++ src/action/ScpFile.java
@@ -0,0 +1,94 @@
+package action;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Properties;
+
+import org.apache.commons.configuration.Configuration;
+import org.apache.commons.configuration.ConfigurationException;
+import org.apache.commons.configuration.PropertiesConfiguration;
+
+import com.jcraft.jsch.ChannelExec;
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.JSchException;
+import com.jcraft.jsch.Session;
+
+public class ScpFile {
+    public static void scp(String srcHost, String srcUser, String srcPw, String srcFilePath,
+            String dstHost, String dstUser, String dstPw, String dstFilePath) throws JSchException, IOException {
+        scp(srcHost, srcUser, srcPw, srcFilePath,
+                dstHost, dstUser, dstPw, dstFilePath, 22);
+    }
+    
+    public static void scp(String srcHost, String srcUser, String srcPw, String srcFilePath,
+            String dstHost, String dstUser, String dstPw, String dstFilePath, int port) throws JSchException, IOException {
+        scpInSrc(srcHost, srcUser, srcPw, srcFilePath,
+                dstHost, dstUser, dstPw, dstFilePath, port);
+    }
+    
+    // first ssh to src host, then execute scp command
+    public static void scpInSrc(String srcHost, String srcUser, String srcPw, String srcFilePath,
+            String dstHost, String dstUser, String dstPw, String dstFilePath, int port) throws JSchException, IOException {
+        String command = String.format("sshpass -p '%s' scp -o StrictHostKeyChecking=no %s %s@%s:%s",
+                dstPw,
+                srcFilePath,
+                dstUser,
+                dstHost,
+                dstFilePath);
+        RemoteExecute.execute(command,
+                srcHost,
+                srcUser,
+                srcPw,
+                22);
+    }
+    
+    // first ssh to dst host, then execute scp command
+    public static void scpInDst(String srcHost, String srcUser, String srcPw, String srcFilePath,
+            String dstHost, String dstUser, String dstPw, String dstFilePath, int port) throws JSchException, IOException {
+        String command = String.format("sshpass -p '%s' scp -o StrictHostKeyChecking=no %s@%s:%s %s",
+                srcPw,
+                srcUser,
+                srcHost,
+                srcFilePath,
+                dstFilePath);
+        RemoteExecute.execute(command,
+                dstHost,
+                dstUser,
+                dstPw,
+                22);
+    }
+    
+    public static void main(String[] args) throws JSchException, IOException, ConfigurationException {
+        Configuration config = new PropertiesConfiguration("host.properties");
+        String srcHost = config.getString("wshost.ip"); 
+        String srcUser = config.getString("wshost.user"); 
+        String srcPw = config.getString("wshost.pw");
+        String srcFilePath = config.getString("wshost.chef_client.path");
+        String dstHost = config.getString("oshost.ip"); 
+        String dstUser = config.getString("oshost.user"); 
+        String dstPw = config.getString("oshost.pw"); 
+//        String dstHost = "114.212.189.117";
+//        String dstUser = config.getString("vm.ubuntu-desktop.user"); 
+//        String dstPw = config.getString("vm.ubuntu-desktop.pw"); 
+//        String dstFilePath = config.getString("dst.chef_client.path");
+        String dstFilePath = "~/";
+        scp(srcHost,
+            srcUser,
+            srcPw,
+            srcFilePath,
+            dstHost,
+            dstUser,
+            dstPw,
+            dstFilePath);
+//        scpInDst(srcHost,
+//                srcUser,
+//                srcPw,
+//                srcFilePath,
+//                dstHost,
+//                dstUser,
+//                dstPw,
+//                dstFilePath,
+//                port);
+    }
+}
